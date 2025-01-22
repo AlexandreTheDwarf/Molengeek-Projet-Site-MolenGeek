f636bdf263b73b9b159502e535c1abcd5a23c7e8
AlexandreTheDwarf <alexandre-vandewiele@outlook.com>
2025-01-22 10:35:35 +0100

    save for break

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+// JavaScript pour le carrousel
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelector(".slides");
+    const totalSlides = document.querySelectorAll(".slide").length;
+    let currentIndex = 0;
+
+    function goToNextSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides; // Passe à la slide suivante ou revient à la première
+        slides.style.transform = `translateX(-${currentIndex * 33}%)`; // Déplace les slides
+    }
+
+    // Change de slide toutes les 15 secondes
+    setInterval(goToNextSlide, 5000);
+});
